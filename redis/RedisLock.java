15c06d9359e1dce8edf78292296e61a72ec26340
zhuowei yang <pewee6@gmail.com>
2020-05-19 00:06:06 +0800

    Update RedisLock.java
    add jedis cluster

--- redis/RedisLock.java
+++ redis/RedisLock.java
@@ -1,12 +1,22 @@
-package redis.distributedlock;
+package com.pewee.test10.config;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.regex.Pattern;
 
 import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
+import redis.clients.jedis.HostAndPort;
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisCluster;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
 /**
@@ -14,48 +24,70 @@ import redis.clients.jedis.JedisPoolConfig;
  * @author pewee
  *
  */
+@Component
 public class RedisLock {
+	
+	static public Logger logger = LoggerFactory.getLogger(RedisLock.class);
 	
 	public static final String LOCK_NAME = "RedisLock_RefreshToken";
 	private static final Long RELEASE_SUCCESS = 1L;
 	
-	static JedisPool jedisPool;
+	private static final String LOCK = "lock";
+	
+	private static final String UNLOCK = "unlock";
+	
+	static JedisCluster jedisCluster ;
 	
 	static String luasrc;
 	
+	private static Pattern p = Pattern.compile("^.+[:]\\d{1,5}\\s*$");  
+	
 	static {
-		String redisIp = "127.0.0.1";
-        int reidsPort = 6379;
-        jedisPool = new JedisPool(new JedisPoolConfig(), redisIp, reidsPort);
-        Jedis jedis = jedisPool.getResource();
-		System.out.println(jedis.ping());
-		jedis.close();
+		JedisPoolConfig poolConfig = new redis.clients.jedis.JedisPoolConfig();
+		poolConfig.setMaxIdle(1000);
+		poolConfig.setMaxWaitMillis(1000);
+		poolConfig.setTestOnBorrow(true);
+		poolConfig.setMaxTotal(1000);
+		poolConfig.setMinIdle(8);
 		
+		String hostAndPort = "127.0.0.1:6380";
+		String[] hostAndPortsArr  = hostAndPort.split(";");
+        Set<HostAndPort> haps = new HashSet<HostAndPort>();  
+        for (String hostport : hostAndPortsArr) {  
+            boolean isIpPort = p.matcher(hostport).matches();  
+            if (!isIpPort) {  
+                throw new IllegalArgumentException("ip 或 port 不合法");  
+            }  
+            String[] ipAndPort = hostport.split(":");  
+            HostAndPort hap = new HostAndPort(ipAndPort[0], Integer.parseInt(ipAndPort[1]));  
+            haps.add(hap);  
+        }   
+		jedisCluster = new JedisCluster(haps, 3000, 1000,3,"Pass@word",poolConfig);
 		try {
 			luasrc =IOUtils.toString(RedisLock.class.getClassLoader().getResourceAsStream("redislock.lua"), 
 					"UTF-8") ;
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		System.out.println(keys(jedisCluster, "*"));
+		System.out.println(jedisCluster.get("{RedisLock_RefreshToken}RedisLock_RefreshToken"));
 	}
 	
 	public static void main(String[] args) {
-		Jedis jedis = jedisPool.getResource();
 		String requestId =Thread.currentThread().getName() + "-" + Thread.currentThread().getId();
-		boolean b = tryLock(jedis, LOCK_NAME, requestId, 30000);
+		boolean b = tryLock(jedisCluster, LOCK_NAME, requestId, 30000);
 		if(b) {
 			System.out.println("主线程成功获取锁");
+			System.out.println("主线程业务处理完成,开始释放锁!");
+	        boolean releaseDistributedLock = RedisLock.releaseLock(jedisCluster, LOCK_NAME, requestId);
+	        if(releaseDistributedLock) {
+	        	 System.out.println("主线程释放锁成功");
+	        } else {
+	        	System.out.println("主线程释放锁失败");
+	        }
 		} else {
 			System.out.println("主线程没有成功获取锁");
 		}
-		System.out.println("主线程业务处理完成,开始释放锁!");
-        boolean releaseDistributedLock = RedisLock.releaseLock(jedis, LOCK_NAME, requestId);
-        if(releaseDistributedLock) {
-        	 System.out.println("主线程释放锁成功");
-        } else {
-        	System.out.println("主线程释放锁失败");
-        }
 	}
 	
 	/**
@@ -66,11 +98,11 @@ public class RedisLock {
      * @param expireTime 锁超期时间,毫秒
      * @return 是否获取成功
      */
-	public static boolean tryLock(Jedis jedis, String lockKey, String requestId, int expireTime) {
+	public static synchronized boolean tryLock(JedisCluster jedisCluster, String lockKey, String requestId, int expireTime) {
 		ArrayList<String> keys = new ArrayList<String>();
-		keys.add("lock");
-		keys.add(lockKey);
-		Object result = jedis.eval(luasrc, keys, Collections.singletonList(requestId));
+		keys.add(getHashKey(lockKey) + LOCK);
+		keys.add(getHashKey(lockKey) + lockKey);
+		Object result = jedisCluster.eval(luasrc, keys, Collections.singletonList(requestId));
 		System.out.println("返回:" + result);
         if (RELEASE_SUCCESS.equals(result)) {
             return true;
@@ -86,11 +118,11 @@ public class RedisLock {
      * @param requestId 请求标识
      * @return 是否释放成功
      */
-    public static boolean releaseLock(Jedis jedis, String lockKey, String requestId) {
+    public static synchronized boolean releaseLock(JedisCluster jedisCluster, String lockKey, String requestId) {
     	ArrayList<String> keys = new ArrayList<String>();
-		keys.add("unlock");
-		keys.add(lockKey);
-    	Object result = jedis.eval(luasrc, keys, Collections.singletonList(requestId));
+		keys.add(getHashKey(lockKey) + UNLOCK);
+		keys.add(getHashKey(lockKey) + lockKey);
+    	Object result = jedisCluster.eval(luasrc, keys, Collections.singletonList(requestId));
     	System.out.println("返回:" + result);
     	if (RELEASE_SUCCESS.equals(result)) {
             return true;
@@ -99,5 +131,33 @@ public class RedisLock {
 
     }
 	
+	private static String getHashKey(String lockKey) {
+		return "{" + lockKey + "}";
+	}
 	
+	public static Set<String> keys(JedisCluster jedisCluster,String pattern) {
+		 
+        logger.debug("Start getting keys... ");
+        TreeSet<String> keys = new TreeSet<>();
+        Map<String, JedisPool> clusterNodes =
+                jedisCluster.getClusterNodes();
+ 
+        for (String key : clusterNodes.keySet()) {
+            logger.debug("Getting keys from: {}", key);
+            JedisPool jedisPool = clusterNodes.get(key);
+            Jedis jedisConn = jedisPool.getResource();
+            try {
+                keys.addAll(jedisConn.keys(pattern));
+            } catch (Exception e) {
+                logger.error("Getting keys error: {}", e);
+            } finally {
+                logger.debug("Jedis connection closed");
+                jedisConn.close();
+            }
+        }
+        logger.debug("Keys gotten");
+ 
+        return keys;
+    }
+
 }
